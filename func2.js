e931a1d7218cd25f0e2af458b83011be29f5c16f
Mr Voiled <voiledCollaboration@gmail.com>
2023-09-08 21:13:12 +0100

    Fixed ad glitch. Adding sections.

--- func2.js
+++ func2.js
@@ -40,7 +40,7 @@ function iconText(){
     iconDesc3.style.display = "none";
     iconDesc4.style.display = "none";
     important.style.display = "none";
-    container.style.gridTemplateColumns = "70px 0px 1fr";
+    container.style.gridTemplateColumns = "70px 1px 1fr 240px";
     icon1.style.justifyContent = "center"
     icon2.style.justifyContent = "center"
     icon3.style.justifyContent = "center"
@@ -63,10 +63,10 @@ function show(){
         let bigLogo = document.getElementById("txtLogo");
         let container = document.getElementById("container");
         let hToSection = document.getElementById("hToSec");
-        let icon1 = document.getElementById("icon1")
-        let icon2 = document.getElementById("icon2")
-        let icon3 = document.getElementById("icon3")
-        let icon4 = document.getElementById("icon4")
+        let icon1 = document.getElementById("icon1");
+        let icon2 = document.getElementById("icon2");
+        let icon3 = document.getElementById("icon3");
+        let icon4 = document.getElementById("icon4");
 
         aside.style.width = "240px";
         iconTextB.style.display = "block";
@@ -79,9 +79,29 @@ function show(){
         iconDesc4.style.display = "block";
         important.style.display = "block";
         // container.style.gridTemplateColumns = "240px 10px";
-        icon1.style.justifyContent = "center"
-        icon2.style.justifyContent = "center"
-        icon3.style.justifyContent = "center"
-        icon4.style.justifyContent = "center"
-        hToSection.style.marginLeft = ""
-}
+        icon1.style.justifyContent = "center";
+        icon2.style.justifyContent = "center";
+        icon3.style.justifyContent = "center";
+        icon4.style.justifyContent = "center";
+        hToSection.style.marginLeft = "";
+};
+
+
+$(document).ready(function () {
+    var cards = $(".card").toArray();
+  
+    for (var i = cards.length - 1; i > 0; i--) {
+      var j = Math.floor(Math.random() * (i + 1)); // Random index between 0 and i (inclusive)
+  
+      // Swap elements
+      var temp = cards[i];
+      cards[i] = cards[j];
+      cards[j] = temp;
+    }
+  
+    // Reorder the shuffled elements within the container
+    $(".scriptSec").empty(); // Remove the old order
+    for (var i = 0; i < cards.length; i++) {
+      $(".scriptSec").append(cards[i]);
+    }
+  });
